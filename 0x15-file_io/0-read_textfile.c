256ecb6b478a35d1825865a386d6c6b748055066
115569 <khozanhlanhla80@gmail.com>
2023-09-10 14:06:45 -0700

    This is the task 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints characters to stdout.
- * @filename: The name of the file to read.
- * @letters: The number of characters to read.
- * Return: The actual number of characters read and printed, or 0 on failure.
+ * read_textfile - reads a text file and prints characters to stdout
+ * @filename: the filename to read
+ * @letters: the number of characters to read
+ * Return: the number of letters it could read and print, or 0 if failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -12,40 +12,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buff;
 	ssize_t bytes_rd, bytes_wr;
 
-	if (filename == NULL)
-	{
-	return (0);
-	}
+	if (!filename)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-	perror("open");
-	return (0);
-	}
+		return (0);
+
 	buff = malloc(sizeof(char) * letters + 1);
-	if (buff == NULL)
-	{
-	perror("malloc");
-	close(fd);
-	return (0);
-	}
+	if (!buff)
+		return (0);
+
 	bytes_rd = read(fd, buff, letters);
 	if (bytes_rd == -1)
-	{
-	perror("read");
-	free(buff);
-	close(fd);
-	return (0);
-	}
+		return (0);
+
 	buff[letters] = '\0';
 	close(fd);
+
 	bytes_wr = write(STDOUT_FILENO, buff, bytes_rd);
 	if (bytes_wr == -1)
-	{
-	perror("write");
+		return (0);
+
 	free(buff);
-	return (0);
-	}
-	free(buff);
+
 	return (bytes_rd);
 }
